8a1835201cf5357f8cd55782a76791cfc4c82e3a
Muhammafjunaid7864 <muhammadjunaid104developer@gmail.com>
2023-11-19 14:58:15 +0500

    again

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "vault"{
-    address=""
-    token=""
+    address="https://vault-cluster-public-vault-d13b0bd2.b4a37a92.z1.hashicorp.cloud:8200"
+    token="hvs.CAESIG4CZwP7dWDl1i0vh8jSaoerGxSLhPCvyHfCZ8IsCcedGicKImh2cy5TMUcwcDRkTW9ad1U0RHFkdndqN3dYTGUuTEhrUm8QygY"
 }
