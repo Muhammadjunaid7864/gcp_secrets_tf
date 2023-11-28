31d105d41449114b9b166fba1d0ea3f87f416322
Muhammadjunaid7864 <muhammadjunaid104developer@gmail.com>
2023-11-29 04:05:13 +0500

    change token

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "vault"{
     address="https://vault-cluster-public-vault-1226856c.9f3a2651.z1.hashicorp.cloud:8200"
-    token="hvs.CAESIMzufKHbQI09rVgoXnrUrfA2W8ZEeM2lXLL3EUIgk2uFGicKImh2cy5hQlpvcWZJdXdJWDY4bXIxWWxDZktBZUYuWlNIOW0QjQE"
+    token="hvs.CAESIP3fRTLQTKdCUsIpJqsOEbCKAywr-kP-XYq2qS47K-oLGicKImh2cy5DNnJVVVVFQnB6RWhOZmpJNndyZnpqRVYuZnV2TlEQ0QE"
 }
